79a03e5be2d6748c4287df95ff3ab9da56d2ac5f
LESTERMATARRITACR <matarritalester329@gmail.com>
2026-03-11 20:57:01 -0600

    Cambios que nada que ver

--- Vehiculos.WEB/Web/Pages/Vehiculos/Eliminar.cshtml.cs
+++ Vehiculos.WEB/Web/Pages/Vehiculos/Eliminar.cshtml.cs
@@ -12,19 +12,22 @@ namespace Web.Pages.Vehiculos
     public class EliminarModel : PageModel
     {
         private IConfiguracion _configuracion;
-        [BindProperty]
-        public VehiculoResponse vehiculo { get; set; } = default!;
+
+        
+
         public EliminarModel(IConfiguracion configuracion)
         {
             _configuracion = configuracion;
         }
+
+        public VehiculoResponse vehiculo { get; set; } = default!;
         public async Task<ActionResult> OnGet(Guid? id)
         {
-            if (id==null)
+            if (id== Guid.Empty)
                 return NotFound();
             string endpoint = _configuracion.ObtenerMetodo("ApiEndPoints", "ObtenerVehiculo");
             var cliente = new HttpClient();
-            
+ 
             var solicitud = new HttpRequestMessage(HttpMethod.Get, string.Format(endpoint, id));
             var respuesta = await cliente.SendAsync(solicitud);
             respuesta.EnsureSuccessStatusCode();
@@ -36,6 +39,9 @@ namespace Web.Pages.Vehiculos
             }
             return Page();
         }
+
+
+
         public async Task<ActionResult> OnPost(Guid? id)
         {
             if (id == Guid.Empty)
